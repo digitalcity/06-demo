ca2ef107b47d4e5c852b05211a9d395447089f59
newming <a977527479@sina.com>
2016-09-18 16:18:20 +0800

    finish

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -14,7 +14,7 @@ function searchGit (add) {
 export { searchGit };
 
 function getJson() {
-  let address = `https://raw.githubusercontent.com/newming/demodata/master/card.json`;
+  let address = `https://raw.githubusercontent.com/newming/demodata/master/card.json?${Math.random()}`;
   return axios.get(address)
     .then( (res) => (
       { getJson:res.data }
